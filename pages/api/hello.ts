0834c3fe3de816a2fdc63e65f0f15687ee66f4b9
seyha26 <seyhachheng26@email.com>
2023-11-17 15:20:19 +0700

    Finished project

---
+++ pages/api/hello.ts
@@ -0,0 +1,4 @@
+import { NextApiRequest, NextApiResponse } from "next";
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  return res.status(200).json("Hello world");
+}
